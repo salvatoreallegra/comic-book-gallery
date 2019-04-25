bca01c1f0fb1833097d8afa29160e001320aa037
Salvatore Allegra <salallegra@gmail.com>
2019-04-25 12:56:19 -0700

    Added comic book model

---
+++ src/ComicBookGallery/ComicBookGallery/Models/ComicBook.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ComicBookGallery.Models
+{
+    public class ComicBook
+    {
+        public int ID { set; get; }
+        public string SeriesTitle { get; set; }
+
+        public int IssueNumber { get; set; }
+        public string DescriptionHtml { get; set; }
+        public Artist[] Artists { get; set; }
+
+        public bool Favorite { get; set; }
+         
+    }
+}
