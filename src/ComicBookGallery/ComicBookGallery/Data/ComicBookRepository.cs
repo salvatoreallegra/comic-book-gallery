f4ee89edf8560951eb0ff93a39766b6bdff63842
Salvatore Allegra <salallegra@gmail.com>
2019-04-25 18:06:36 -0700

    Add Index view

--- src/ComicBookGallery/ComicBookGallery/Data/ComicBookRepository.cs
+++ src/ComicBookGallery/ComicBookGallery/Data/ComicBookRepository.cs
@@ -58,7 +58,10 @@ namespace ComicBookGallery.Data
             Favorite = false
      }
  };
-
+        public ComicBook[] GetComicBooks()
+        {
+            return _comicBooks;
+        }
         public ComicBook GetComicBook(int id)
         {
             ComicBook comicBookToReturn = null;
